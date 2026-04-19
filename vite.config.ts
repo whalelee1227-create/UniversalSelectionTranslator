4d4b79d1632f271bab3df7f43aad959df36f3d71
whalelee <whalelee@whaleleedeMac-mini.local>
2026-04-19 15:37:30 +0800

    chore: project scaffolding

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: 'src/renderer',
+  base: './',
+  build: {
+    outDir: '../../dist/renderer',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
